cbc947cebdd638c8f6d8425a418e76193c41f677
bluezipGC <clareg1@mymail.nku.edu>
2023-02-23 19:05:17 -0500

    WeatherAPI
    WeatherAPI

--- firstScript.js
+++ firstScript.js
@@ -12,3 +12,14 @@ for (var i = 0; i < list.length; i++) {
 function alertOnButton(largest) {
     alert(largest);
 }
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.weather.gov/points/39.7456,-97.0892");
+xhr.send()
+xhr.onload = function() {
+    coolButton(`Loaded: ${xhr.status} ${xhr.response}`);
+  };
+
+  function coolButton() {
+    alert(`Loaded: ${xhr.status} ${xhr.response}`);
+  }
